9730759f67b266660c88637192fc5b1bb459b2b9
Davi <88855441+davi12345452@users.noreply.github.com>
2023-03-21 13:43:31 -0300

    t1 pronto

--- EstruturasDeDados/t1/t1.c
+++ EstruturasDeDados/t1/t1.c
@@ -7,7 +7,7 @@ typedef struct {
     float media;
 } aluno_t;
 
-int le_arq(int n, aluno_t alunos[n], char *nome_do_arquivo) {
+int le_arq(aluno_t **alunos, char *nome_do_arquivo) {
     FILE *file = fopen(nome_do_arquivo, "r");
     if (!file) {
         return -1;
@@ -16,13 +16,15 @@ int le_arq(int n, aluno_t alunos[n], char *nome_do_arquivo) {
     int num_alunos;
     fscanf(file, "%d", &num_alunos);
 
-    if (num_alunos != n) {
+    *alunos = (aluno_t *)malloc(num_alunos * sizeof(aluno_t));
+    if (!*alunos) {
         fclose(file);
         return -1;
     }
 
-    for (int i = 0; i < n; i++) {
-        fscanf(file, "%d %f %f", &alunos[i].matricula, &alunos[i].nota1, &alunos[i].nota2);
+    for (int i = 0; i < num_alunos; i++) {
+        fscanf(file, "%d %f %f", &(*alunos)[i].matricula, &(*alunos)[i].nota1, &(*alunos)[i].nota2);
+        (*alunos)[i].media = ((*alunos)[i].nota1 + (*alunos)[i].nota2) / 2;
     }
 
     fclose(file);
@@ -43,25 +45,19 @@ void grava_arq(int n, aluno_t alunos[n], char *nome_do_arquivo) {
     fclose(file);
 }
 
-void calcula_medias(int n, aluno_t alunos[n]) {
-    for (int i = 0; i < n; i++) {
-        alunos[i].media = (alunos[i].nota1 + alunos[i].nota2) / 2;
-    }
-}
-
 int main() {
     char *nome_do_arquivo = "notas.txt";
-    int n = 15;
-    aluno_t alunos[n];
+    aluno_t *alunos;
 
-    int num_alunos = le_arq(n, alunos, nome_do_arquivo);
+    int num_alunos = le_arq(&alunos, nome_do_arquivo);
     if (num_alunos == -1) {
         printf("Erro ao ler o arquivo.\n");
         return 1;
     }
 
-    calcula_medias(n, alunos);
-    grava_arq(n, alunos, nome_do_arquivo);
+    grava_arq(num_alunos, alunos, nome_do_arquivo);
 
+    free(alunos);
     return 0;
 }
+
